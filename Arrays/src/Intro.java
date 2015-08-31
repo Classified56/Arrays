9d758620f934fe7ace182ff96e709168435d0daa
Classified56 <davis.ranney@MU-C1-01.mullen.pvt.k12.co.us>
2015-08-31 14:19:23 -0600

    Challenge 1 done

--- Arrays/src/Intro.java
+++ Arrays/src/Intro.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
 
 public class Intro
 	{
+		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
+			{
+				System.out.print("How many sweets do you like? ");
+				int numSweet = userInput.nextInt();
+				if(numSweet > 0)
+					{
+						String candyArray[] = new String[numSweet];
+						Scanner sweetName = new Scanner(System.in);
+						for(int i = 0; i < numSweet; i++)
+							{
+								System.out.print("What sweet do you like? ");
+								String sweet = sweetName.nextLine();
+								candyArray[i] = sweet;
+							}
+						for(String sweets : candyArray){
+							System.out.println("You like " + sweets);
+						}
+					}
+				else
+					{
+						System.out.println("You have a very healthy diet. ");
+					}
+			}
+		
+		public static void inClass()
 			{
 				int myArray[] = new int[5];
 				for(int i = 0; i < myArray.length; i++)
